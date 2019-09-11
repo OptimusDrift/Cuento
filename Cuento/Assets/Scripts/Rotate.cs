e6c48397d9e6a46ae8775ead6d80ccb77d33bd0d
optimusdrift <optimusdrift@gmail.com>
2019-09-11 03:45:40 -0300

    Primer subida

---
+++ Cuento/Assets/Scripts/Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private Transform ObjetoARotar;
+    public float Velocidad;
+    private bool rebote;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjetoARotar = GetComponent<Transform>();
+        rebote = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ObjetoARotar.rotation.x <= 0.8f && !rebote)
+        {
+            ObjetoARotar.Rotate(Velocidad * Time.deltaTime, ObjetoARotar.rotation.y, ObjetoARotar.rotation.z);
+        }
+        if (ObjetoARotar.rotation.x >= 0.8f)
+        {
+            rebote = true;
+        }
+        if (rebote && ObjetoARotar.rotation.x >= 0.7f)
+        {
+            ObjetoARotar.Rotate(-Velocidad * Time.deltaTime, ObjetoARotar.rotation.y, ObjetoARotar.rotation.z);
+        }
+    }
+}
